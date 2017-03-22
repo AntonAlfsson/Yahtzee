9225b6050ce1a2d6f67f01a7b2bedec3123baa4e
AntonAlfsson <Anton.Alfsson@gmail.com>
2017-03-22 14:49:52 +0100

    Lagt till Grunden

---
+++ js/classes/base-and-utility-classes/jquery-dom-events.class.js
@@ -0,0 +1,41 @@
+class JqueryDomEvents extends Array {
+
+  constructor(){
+    super();
+
+    this.push.apply(this,[
+      'blur',
+      'change',
+      'click',
+      'data-click',
+      'contextmenu',
+      'dblclick',
+      'error',
+      'focus',
+      'focusin',
+      'focusout',
+      'hover',
+      'keydown',
+      'keypress',
+      'keyup',
+      'load',
+      'mousedown',
+      'mouseenter',
+      'mouseleave',
+      'mousemove',
+      'mouseout',
+      'mouseover',
+      'mouseup',
+      'ready',
+      'resize',
+      'scroll',
+      'select',
+      'submit',
+      'toggle',
+      'trigger',
+      'unload'
+    ]);
+    
+  }
+
+}
