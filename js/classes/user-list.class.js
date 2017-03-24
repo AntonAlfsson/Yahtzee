497b7450a7f4f269e24ea821e4caf5d1c1de8876
AntonAlfsson <Anton.Alfsson@gmail.com>
2017-03-24 13:48:00 +0100

    kan nu hämta users från DB samt skapa nya

--- js/classes/user-list.class.js
+++ js/classes/user-list.class.js
@@ -6,7 +6,27 @@ class UserList extends List {
     }
 
     createUsers(){
-        var name = window.prompt("Username: ","Username"); // frågar efter namn och tar emot det
+        do{
+            var name = window.prompt("Username: ","Username"); // frågar efter namn och tar emot det
+
+            this.db.searchUser([name], (data)=>{
+               
+                if(!data.length){ // om det ej finns något i DB
+                    this.db.newUser({ // skapas en ny användare i DB
+                        userName: name
+                    });
+                    //this.push(new User(name)); // skapar objekt av användare
+
+                }else{
+                    //this.push(new User(data[0].userName));  // skapar ett objekt av user från DB
+                }
+
+
+            });
+
+            var b1 = window.confirm("Another user?"); // frågar efter ytterligare users om nej b1 = false och loopen bryts
+        }while(b1);
+
 
     }
 
@@ -20,11 +40,11 @@ class UserList extends List {
     static get sqlQueries(){
         return {
             newUser: `
-                INSERT userName SET ?
-                `,
+INSERT User SET ?
+`,
             searchUser: `
-                Select * FROM User WHERE userName = ?
-                `
+Select * FROM User WHERE userName = ?
+`
         }
     }
 
