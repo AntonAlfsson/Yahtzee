650504d37824dbf4b931c118ac874f1ecb2ca41b
Anna Fock <anna@fock.se>
2017-03-25 16:28:56 +0100

    Skapat metoden totalSum i klassen User

--- js/classes/user.class.js
+++ js/classes/user.class.js
@@ -9,9 +9,10 @@ class User extends Base {
 
 		this.scoreList = [];
 
-		this.startScoreList(); //Hämtar metod för att skapa listan med 18 tomma platser
+		//this.startScoreList(); //Hämtar metod för att skapa listan med 18 tomma platser
 
 	}
+
 
 	//Kanske ett sätt att få en lista med 18 tomma platser?
 	startScoreList(){
@@ -36,4 +37,38 @@ class User extends Base {
 			}	
 		}
 	}
+
+	//Räknar ut bonussumma och lägger till 50 när summan blir över 63
+	totalSum(){
+
+		var bonusSum = 0;
+		var totalSum = 0;
+
+		//Räknar ut bonussumma
+		for (var i = 0; i < this.scoreList.length-12; i++){
+			bonusSum = bonusSum + this.scoreList[i];
+		}
+
+		//Lägger till totalen för bonussumman i arrayen 
+		this.scoreList[6] = bonusSum;
+
+		//Delar ut bonus om bonussumman är över 63
+		if (bonusSum >= 63){			
+			this.scoreList[7] = 50;
+		}
+		else{
+			this.scoreList[7] = 0;
+		}
+
+		//Räknar ut totalsumman
+		for (var j = 6; j < this.scoreList.length; j++){
+			totalSum = totalSum + this.scoreList[j	];
+		}
+
+		//Lägger till totalsumman i arrayen
+		this.scoreList[17] = totalSum;
+
+		return totalSum;
+	}
+
 }
